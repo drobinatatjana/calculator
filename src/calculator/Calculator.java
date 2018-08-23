df10766e85c28d4a0558531c115018c0fa070584
St61199 <drobina.tatjana@gmail.com>
2018-08-23 20:28:50 +0300

    what we done : calculate 2 expressions

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,57 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
-    }
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
 
+
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+
+
+        double result1;
+        switch (expression[1]) {
+            case "+":
+                result1 = a + b;
+                break;
+            case "-":
+                result1 = a - b;
+                break;
+            case "*":
+                result1 = a * b;
+                break;
+            case "/":
+                result1 = a / b;
+                break;
+
+            default:
+                return "ERROR";
+        }
+        if (expression.length < 4) {
+            return String.valueOf(result1);
+        }
+        System.out.println(expression[3]);
+        System.out.println(expression[4]);
+
+        double c = Double.parseDouble(expression[4]);
+        double result2;
+        switch (expression[3]) {
+            case "+":
+                result2 = result1 + c;
+                break;
+            case "-":
+                result2 = result1 - c;
+                break;
+            case "*":
+                result2 = result1 * c;
+                break;
+            case "/":
+                result2 = result1 / c;
+                break;
+            default:
+                return "ERROR";
+        }
+        return String.valueOf(result2);
+    }
 }
